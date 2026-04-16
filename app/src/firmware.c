3666c9ffb622722a75fa31663c86830de1ad650d
Akshada Joshi <akshada.joshi@lk-ea.com>
2026-04-16 20:09:13 +0530

    implementing systick timers

--- app/src/firmware.c
+++ app/src/firmware.c
@@ -1,8 +1,14 @@
 #include <libopencm3/stm32/rcc.h>
 #include <libopencm3/stm32/gpio.h>
+#include <libopencm3/cm3/systick.h>
+#include <libopencm3/cm3/vector.h>
+
 
 #define LED_PORT (GPIOA)
 #define LED_PIN  (GPIO5)
+
+#define CPU_FREQ 84000000
+#define SYSTICK_FREQ 1000
 
 static void rcc_setup(void) {
   rcc_clock_setup_pll(&rcc_hsi_configs[RCC_CLOCK_3V3_84MHZ]);
@@ -20,19 +26,43 @@ static void gpio_setup(void) {
   gpio_mode_setup(LED_PORT, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, LED_PIN);
 }
 
-static void delay_cycles(uint32_t cycles) {
+static void systick_setup(void)
+{
+  systick_set_frequency(SYSTICK_FREQ, CPU_FREQ); /* Set systick to trigger every 1ms */
+  systick_counter_enable(); /* Start the systick counter */
+  systick_interrupt_enable(); /* Enable systick interrupt */
+}
+volatile uint32_t tick_count = 0;
+void sys_tick_handler(void)
+{
+  tick_count++;
+}
+
+static uint64_t get_ticks(void) {
+  return tick_count;
+}
+
+#if 0 /*this function creates busy-wait delay*/
+static void delay_cycles(uint32_t cycles) { /*Using this function was consuming CPU cycles alternate is doing it with systick - pheriphersl will time keeping and CPU is free */
   for (uint32_t i = 0; i < cycles; i++) {
     __asm__("nop");
   }
 }
+#endif
 
 int main(void) {
   rcc_setup();
   gpio_setup();
+  systick_setup();
 
+  uint64_t last_tick = get_ticks();
   while (1) {
-    gpio_clear(LED_PORT, LED_PIN);
-   // delay_cycles(84000000 / 4);
+
+    if (get_ticks() - last_tick >= 500) 
+    { /* Toggle LED every 2 seconds */
+      last_tick = get_ticks();
+      gpio_toggle(LED_PORT, LED_PIN);
+    }
   }
 
   // Never return
